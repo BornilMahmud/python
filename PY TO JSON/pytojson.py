a5de977029ddc6b3b1e8cbfe8fd3a20ed455fcad
Bornil Mahmud <bornilprof@gmail.com>
2026-05-06 07:32:58 +0600

    Update PY TO JSON/pytojson.py via BM Compiler

---
+++ PY TO JSON/pytojson.py
@@ -0,0 +1,13 @@
+import json
+# python object to json 
+
+bornil = {
+  "name": "bornil mahmud ",
+  "age " : 22,
+  "city" : "khoksa",
+  "status" : False, 
+  "play" : ["football","cricket","badmintion"]
+}
+
+result = json.dumps(bornil,indent=4)
+print(result)
